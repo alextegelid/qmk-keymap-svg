16febd6a942aff8e8e916fea39899b004b7ab85a
Alexander Tegelid <alex@humblegiant.se>
2022-06-01 09:23:49 +0200

    Increase overall default size

--- settings.py
+++ settings.py
@@ -4,17 +4,17 @@ hand_thumbs_cols = 2
 
 debug = False
 
-key_w = 60
-key_h = 45
-key_rx = 3
-key_ry = 3
-inner_pad_w = 2
-inner_pad_h = 2
+key_w = 80
+key_h = 65
+key_rx = 5
+key_ry = 5
+inner_pad_w = 5
+inner_pad_h = 5
 outer_pad_w = 40
 outer_pad_h = 40
 
-font_size = 11
-line_spacing = font_size * 1.1
+font_size = 16
+line_spacing = font_size * 1.2
 
 # Auto capitalize keycodes that is not in the labels list.
 # Keycodes that includes an underscore will not be capitalized.
